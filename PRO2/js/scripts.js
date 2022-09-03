c928ac9f37ba216e330f9a91a8ab1781d3ae2b1f
josepmartorell <martorelljosep>
2022-09-03 09:48:57 +0200

    upload exercise 2

---
+++ PRO2/js/scripts.js
@@ -0,0 +1,7 @@
+function MyScript() {
+    //este mensaje sale por consola
+    console.log("Hola Mundo! Que fácil es inccluir 'comillas simples'" + ' "y comillas dobles"')
+    //y este mensaje a través de un elemento de la pagina index.html
+    let message ="Hola Mundo! Que fácil es inccluir 'comillas simples'" + ' "y comillas dobles"'
+    alert(message);
+}
