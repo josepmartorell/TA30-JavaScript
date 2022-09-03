5b95cb6c2716ac7432b38fc97351924968ac35e9
josepmartorell <martorelljosep>
2022-09-03 10:10:05 +0200

    upload exercise 3

---
+++ PRO3/js/scripts.js
@@ -0,0 +1,6 @@
+function MyArray() {
+    let array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    array.forEach(element => console.log(element));
+    
+}
